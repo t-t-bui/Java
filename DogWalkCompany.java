1ee73c08f7c9d9ff8e0a9f017b9131c66d5e6c74
t-t-bui <tbui931958@gmail.com>
2026-02-04 14:12:13 -0600

    UIL coding

---
+++ DogWalkCompany.java
@@ -0,0 +1,13 @@
+public class DogWalkCompany {
+    public int numAvailableDogs(int hour){
+        // return the number of dogs, always greater than 0, 
+        // that are available for a walk during the time specified by hour
+
+        return 0;
+    }
+
+    public void updateDogs(int hour, int numberDogsWalked){
+        // Decreases, by numberDogsWalked, the number of dogs available 
+        // for a walk during the time specified by hour
+    }
+}
